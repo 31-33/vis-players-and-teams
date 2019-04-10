7893dd02d5c34a66b88027c8dd5dca86985b1687
Chris Symon <chris.symon97@gmail.com>
2019-04-10 20:13:31 +1200

    added teams

--- create_dataset/index.js
+++ create_dataset/index.js
@@ -1,10 +1,31 @@
 var sqlite3 = require('sqlite3').verbose();
 var Player = require('./player');
+const randomNormal = require('random-normal');
 
 let db = new sqlite3.Database('./dataset.db', err => {
   if(err) return console.log(`Error: ${err}`);
   console.log('Initialized SQlite database');
 });
+
+db.run(`CREATE TABLE teams (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        skill_level REAL)`);
+var teamNames = ["The Incredible Horses",
+  "The Peaceful Ants",
+  "The Great Tigers",
+  "The Giddy Cheetahs",
+  "The Tasteful Ferrets",
+  "The Beneficial Hyenas",
+  "The Lonely Baboons",
+  "The Reminiscent Crocodiles",
+  "The Smelly Meerkats",
+  "The Encouraging Deers"];
+for(j = 0; j < teamNames.length; j++){
+  db.run('INSERT INTO teams (id, name, skill_level) VALUES (?, ?, ?)',
+    [j, teamNames[j], randomNormal({ mean: 3, dev: 1})]);
+}
+
 
 db.run(`CREATE TABLE players (
         id INTEGER PRIMARY KEY,
@@ -20,6 +41,8 @@ setInterval(() => {
       db.run('INSERT INTO players (id, name, avatar_path, skill_level) VALUES (?, ?, ?, ?)',
         [player.id, player.player_name, player.avatar_path, player.skill_level]);
     })
-}, 5000);
+}, 100);
+
+
 
 // db.close();
